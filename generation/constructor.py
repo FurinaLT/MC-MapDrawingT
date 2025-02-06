b4041f79cb570fb549b1a3c61feba10a16a88583
LY-Xiang <74460076+LY-Xiang@users.noreply.github.com>
2025-02-06 18:08:15 +0800

    Lint & Format

--- generation/constructor.py
+++ generation/constructor.py
@@ -1,4 +1,14 @@
+# pylint: disable = C0301, R0903
+"""
+定义地图与箱子的简单构造对象和构造器
+"""
+
+
 class Map:
+    """
+    简单地图构造对象
+    """
+
     TEMPLATE = '{{Count:1b,Slot:{Slot}b,id:"minecraft:filled_map",tag:{{map:{MapId}}}}}'
 
     def __init__(self, Slot: int, MapId: int) -> None:
@@ -10,6 +20,10 @@ class Map:
 
 
 class Box:
+    """
+    简单箱子构造对象
+    """
+
     TEMPLATE = '{{Count:1b,{Slot}id:"minecraft:chest",tag:{{BlockEntityTag:{{Items:{Items},id:"minecraft:chest"}},display:{{Name:\'{{"Italic":false,"Extra":[{{"text":""}},{{"color":"dark_gray","text":"["}},{{"color":"dark_aqua","text":"{Begin}"}},{{"color":"gray","text":","}},{{"color":"aqua","text":"{End}"}},{{"color":"dark_gray","text":")"}}],"text":""}}\'}}}}}}'
 
     def __init__(self, Begin: int, End: int, Slot: int = -1) -> None:
@@ -30,4 +44,8 @@ class Box:
 
 
 class Constructor(Box):
-    pass
+    """
+    构造器
+    继承自 Box
+    完全等同
+    """
